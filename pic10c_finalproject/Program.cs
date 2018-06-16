7bb7e38eac5651f0e8fa78f2e76554b3c2f16427
alepelayo <alepelayo@g.ucla.edu>
2018-06-16 01:50:09 -0700

    starting getters while private are all strings

--- pic10c_finalproject/Program.cs
+++ pic10c_finalproject/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace pic10c_finalproject
 {
 
+    public enum suit_t {OROS, COPAS, ESPADAS, BATOS};
+    public enum rank_t {AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA, CABALLO, REY};
+
     class Card
     {
 
@@ -23,30 +26,53 @@ namespace pic10c_finalproject
             
             switch(s)
             {
-                case 1: suit = "OROS"; break;
-                case 2: suit = "COPAS"; break;
-                case 3: suit = "ESPADA"; break;
-                case 4: suit = "BATOS"; break;
+                case 1: suit = "oros"; break;
+                case 2: suit = "Copas"; break;
+                case 3: suit = "Espadas"; break;
+                case 4: suit = "Batos"; break;
                 default: break;
             }
 
             switch(r)
             {
-                case 1: rank = "AS"; break;
-                case 2: rank = "DOS"; break;
-                case 3: rank = "TRES"; break;
-                case 4: rank = "CUATRO"; break;
-                case 5: rank = "CINCO"; break;
-                case 6: rank = "SEIS"; break;
-                case 7: rank = "SIETE"; break;
-                case 8: rank = "SOTA"; break;
-                case 9: rank = "CABALLO"; break;
-                case 10: rank = "REY"; break;
+                case 1: rank = "As"; break;
+                case 2: rank = "Dos"; break;
+                case 3: rank = "Tres"; break;
+                case 4: rank = "Cuatro"; break;
+                case 5: rank = "Cinco"; break;
+                case 6: rank = "Seis"; break;
+                case 7: rank = "Siete"; break;
+                case 8: rank = "Sota"; break;
+                case 9: rank = "Caballo"; break;
+                case 10: rank = "Rey"; break;
                 default: break;
             }
         }
 
+        public string get_spanish_suit
+        {
+            
+            get
+            {
+                return suit;
+            }
+
+        }
+
+        public string get_spanish_rank
+        {
+            get
+            {
+                return rank;
+            }
+        }
+
+       
+
     }
+}
+
+
 
 
 
@@ -55,10 +81,9 @@ namespace pic10c_finalproject
         static void Main(string[] args) //this is static so that we do not have to make an object 
                                         //Program every time we want to want to use any of the member fields, properties or methods
         {
-
+            Card myCard = new Card();
+            myCard.get_spanish_rank();
             Console.Read();
         }
     }
 
-
-}
